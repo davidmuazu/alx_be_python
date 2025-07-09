868350b49ea3a9af9d03a5ebb813c9232271a7ae
Muazu David Joseph <josephmuazu5@gmail.com>
2025-07-09 19:57:07 +0100

    Add files via upload

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+principal = 1000
+rate = 0.05
+time = 3
+#the formula for cal culating simple intrest is I = P * T * R
+interest = principal * rate * time
+print("The simple interest is:", interest)
