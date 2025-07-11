dfb1bcceff13952307a59c9d08f435f1a4200939
Muazu David Joseph <josephmuazu5@gmail.com>
2025-07-11 11:02:53 +0100

    Update hours_to_seconds.py
    removed brackets

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,3 +1,3 @@
 hours = 2
-seconds = (3600 * hours) #this formular converts hours to seconds
+seconds = 3600 * hours #this formular converts hours to seconds
 print(hours, "hour(s) is ", seconds, "seconds")
