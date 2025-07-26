3f9b4ba1b784c3f8f8edb3188a62bec58de9a0ed
Muazu David Joseph <josephmuazu5@gmail.com>
2025-07-26 09:02:50 +0100

    Shopping List Manager
    Utilize Python lists to create a simple shopping list manager that allows users to add items, view the current list, and remove items.

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,3 +1,46 @@
-shopping_list = []
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
 
-shopping_list.__add__
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+        item = ""
+        if choice == '1':
+            # Prompt for and add an item
+            item = input("Enter item name: ").strip()
+            if item in shopping_list:
+                print(f"{item} already in shopping list")
+            else:
+                shopping_list.append(item)
+                print(f"{shopping_list}, {item} was added")
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            item = input("Enter item name: ").strip()
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{shopping_list}, {item} was removed")
+            else:
+                print(f"{item} not found in shopping list")
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            #print(f"{shopping_list}")
+            print("\nYour shopping list:")
+            for lst, item in enumerate(shopping_list, start=1):
+                print(f"{lst}. {item}")
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
