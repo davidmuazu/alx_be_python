367588bb5501f90255537eede25c1d5ccc0c786b
Muazu David Joseph <josephmuazu5@gmail.com>
2025-07-27 00:02:25 +0100

    Explore `datetime` Module
     Familiarize yourself with Python’s datetime module by writing a script that performs specified operations with dates and times.

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,21 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.today()
+    #Format and print the current date and time in a readable format, such as “YYYY-MM-DD HH:MM:SS”.
+    current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {current_date}")
+display_current_datetime()
+
+
+future_day = int(input("Enter the number of days to add to the current date: "))
+def calculate_future_date():
+    global future_day
+    future_date = ""
+    today = datetime.datetime.today()
+    future_date = today + datetime.timedelta(days=future_day)
+    #Print the future date in a format like “YYYY-MM-DD”.
+
+    print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
+
+calculate_future_date()
